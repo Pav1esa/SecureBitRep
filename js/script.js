c5d96aaad99e6695b255e4daa939a3caf4676d0f
Pav1esa <110695473+Pav1esa@users.noreply.github.com>
2023-12-14 20:27:45 +0200

    add func to show content based on menu selection

--- js/script.js
+++ js/script.js
@@ -10,3 +10,35 @@ let balance = 0;
             document.getElementById("userBalance").innerText = balance + ' ETH';
         }
     }
+    // Function to show content based on menu selection
+    function showContent(option) {
+    document.getElementById("content").style.display = "none";
+    document.getElementById("balanceContainer").style.display = "none";
+    document.getElementById("sendContainer").style.display = "none";
+    document.getElementById("receiveContainer").style.display = "none";
+    document.getElementById("historyContainer").style.display = "none";
+
+    switch (option) {
+        case 'Home':
+            document.getElementById("content").style.display = "block";
+            break;
+        case 'Balance':
+            generateRandomBalance();
+            document.getElementById("balanceContainer").style.display = "block";
+            document.getElementById("userBalance").innerText = balance + ' ETH'; // Оновлено рядок
+            break;
+        case 'Send':
+            document.getElementById("sendContainer").style.display = "block";
+            break;
+        case 'Receive':
+            document.getElementById("receiveContainer").style.display = "block";
+            document.getElementById("receiveAddress").innerText = receiveAddress;
+            break;
+        case 'History':
+            displayTransactionHistory();
+            document.getElementById("historyContainer").style.display = "block";
+            break;
+        default:
+            document.getElementById("content").style.display = "block";
+    }
+}
